7ef8ae6b480fa3f7f2ae8f13ddd743a468dad328
Will LIU Yanwei <willliu1995@outlook.com>
2019-03-11 22:26:51 +0800

    朴素贝叶斯搞定~顺便修复了一些问题
    - 针对Python3进行了适配；（如pkl文件的binary读取，sklearn.cross_validation已经调整等问题）
    - 移除了一些不必要的代码(cPickle)，
    - 修复了在我的环境中存在的相对路径问题
    - 完成了朴素贝叶斯的练习

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -9,25 +9,34 @@
     Sara has label 0
     Chris has label 1
 """
-    
+import os
 import sys
 from time import time
-sys.path.append("../tools/")
-from email_preprocess import preprocess
-
+sys.path.append(os.getcwd()) #将整个工作区目录添加为包搜索范围
+sys.path.insert(0, "./tools") # 将tool文件夹设为最高搜索优先级
+# 上两行代码灵感来自： https://blog.csdn.net/dcrmg/article/details/79546962
+from tools.email_preprocess import preprocess
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
+t0 = time() # 初始化计时器
 
+def NBaccuracy(features_train, labels_train, features_test, labels_test):
+    from sklearn.naive_bayes import GaussianNB
+    clf = GaussianNB()
+    clf.fit(features_train, labels_train)
+    accuracy = clf.score(features_test, labels_test)
+    print("accuracy: {}".format(accuracy))
+    return accuracy
 
+NBaccuracy(features_train, labels_train, features_test, labels_test)
+
+print("training time:", round(time()-t0, 3), "s") #打印训练时间
 #########################################################
 
 
